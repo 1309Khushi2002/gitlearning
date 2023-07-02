c69b917afb13d97cdf56f3e9584295fb601405df
1309Khushi2002 <105561579+1309Khushi2002@users.noreply.github.com>
2023-07-02 23:29:39 +0530

    Revert "Adding Javascript file"
    This reverts commit c3fd1cec6de4ef0d71c51f4cdd19346ea2f47eea.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 
 function module2(){
     console.log("Hello world2");
+=======
+function data(){
+    console.log("hello world");
+>>>>>>> parent of c3fd1ce (Adding Javascript file)
 }
